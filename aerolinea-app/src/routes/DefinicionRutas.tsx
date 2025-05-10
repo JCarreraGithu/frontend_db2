21ce62fa4c15b0cbd8fdb2a5bd95f683eea5fe4e
Marlon Lopez <86318634+Marlon599@users.noreply.github.com>
2025-05-10 12:14:57 -0600

    Update DefinicionRutas.tsx

--- aerolinea-app/src/routes/DefinicionRutas.tsx
+++ aerolinea-app/src/routes/DefinicionRutas.tsx
@@ -2,7 +2,6 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../autenticacion/pages/login';
 import Inicio from '../paginas/Inicio';
 import Dashboard from '../Dashboard/pages/Dashboard';
-import ListaVuelos from '../modulos/gestion de vuelos/pages/ListaVuelos';
 
 export default function DefinicionRutas() {
   const isAuthenticated = !!localStorage.getItem('token'); // Verifica si hay un token en el localStorage
@@ -13,7 +12,6 @@ export default function DefinicionRutas() {
       <Route path="/dashboard" element={<Dashboard />} />
       {/* Ruta protegida para el Dashboard */}
       <Route path="/dashboard" element={isAuthenticated ? <Dashboard /> : <Navigate to="/login" />} />
-      <Route path="/vuelos" element={<ListaVuelos />} />
       <Route path="/inicio" element={<Inicio />} />
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
